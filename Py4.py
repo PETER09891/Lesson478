0f36fa178e61d04458ba9705a2c7870f4f52dc79
obafemitemi.p@gmail.com <obafemitemi.p@gmail.com>
2026-01-04 18:22:44 +0100

    j89

--- Py4.py
+++ Py4.py
@@ -1,1 +1,8 @@
-n = int(input("Enter "))
+n = int(input("Enter a number: "))
+for i in range(2,n+1):
+    for j in range (2,(int(i**0.5)+1)):
+        if i%j==0:
+            print (f"{i} Not a prime number")
+            break
+    else:
+        print (f"{i} is a prime number")
